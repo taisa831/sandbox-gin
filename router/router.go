a758923b1dbe7d41015cff36dbf9050e68181f5e
taisa <g5.taisa831@gmail.com>
2019-07-25 19:19:53 +0900

    修正

--- router/router.go
+++ router/router.go
@@ -17,11 +17,11 @@ func Router(dbConn *gorm.DB) {
 
   r.LoadHTMLGlob("templates/*")
 
-  r.GET("/go/todo", todoHandler.GetAll) // 一覧画面
-  r.POST("go/todo", todoHandler.CreateTask) // 新規作成
-  r.GET("/go/todo/:id", todoHandler.EditTask) // 編集画面
-  r.POST("/go/todo/edit/:id", todoHandler.UpdateTask) // 更新
-  r.POST("/go/todo/delete/:id", todoHandler.DeleteTask) // 削除
+  r.GET("/todo", todoHandler.GetAll) // 一覧画面
+  r.POST("todo", todoHandler.CreateTask) // 新規作成
+  r.GET("/todo/:id", todoHandler.EditTask) // 編集画面
+  r.POST("/todo/edit/:id", todoHandler.UpdateTask) // 更新
+  r.POST("/todo/delete/:id", todoHandler.DeleteTask) // 削除
 
   apiV1 := r.Group("/api/v1")
   {
@@ -29,11 +29,11 @@ func Router(dbConn *gorm.DB) {
       Db: dbConn,
     }
 
-    apiV1.GET("/go/todo", apiTodoHandler.GetAll)
-    apiV1.POST("/go/todo", apiTodoHandler.CreateTask)
-    apiV1.GET("/go/todo/:id", apiTodoHandler.EditTask)
-    apiV1.POST("/go/todo/edit/:id", apiTodoHandler.UpdateTask)
-    apiV1.POST("/go/todo/delete/:id", apiTodoHandler.DeleteTask)
+    apiV1.GET("/todo", apiTodoHandler.GetAll)
+    apiV1.POST("/todo", apiTodoHandler.CreateTask)
+    apiV1.GET("/todo/:id", apiTodoHandler.EditTask)
+    apiV1.POST("/todo/edit/:id", apiTodoHandler.UpdateTask)
+    apiV1.POST("/todo/delete/:id", apiTodoHandler.DeleteTask)
   }
 
   r.Run(":9000")
